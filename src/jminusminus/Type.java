2219c5cd6b640e06800a89eba3f938aa2061c637
Casper Skjærris <casper_skj@live.dk>
2020-03-05 12:16:10 +0000

    Added static instance block and interface parsing support (#24)

--- src/jminusminus/Type.java
+++ src/jminusminus/Type.java
@@ -99,6 +99,8 @@ class Type {
      */
     public final static Type CONSTRUCTOR = new Type(null);
 
+    public final static Type STATIC_BLOCK = new Type(null);
+
     /**
      * The "any" type (denotes wild expressions).
      */
@@ -466,6 +468,9 @@ class Type {
      */
 
     private static String toJava(Class classRep) {
+        if(classRep == null){
+            return "Null";
+        }
         return classRep.isArray() ? toJava(classRep.getComponentType()) + "[]"
                 : classRep.getName();
     }
--- tests/pass/ReservedWords.java
+++ tests/pass/ReservedWords.java
@@ -1,16 +1,31 @@
-//public class ReservedWords extends SomeClass implements  SomeInterface {
-//    public static void main(String[] args) {
-//        do {
-//         //   /* do nothing */
-//        } while (true);
-//        for (;;) {
-//            try {
-//                if (true) { continue; }
-//            }
-//            catch (SomeException e) {
-//         //       /* do nothing */
-//            }
-//        }
-//        final int x;
-//    }
-//}
+package pass;
+
+class SomeClass{
+
+}
+interface SomeInterface{
+
+}
+class SomeException extends Exception{
+
+}
+
+public class ReservedWords extends SomeClass implements SomeInterface {
+    //TODO: Uncomment stuff as they are added for parsing support
+    public static void main(String[] args) {
+      //  do {
+         //   /* do nothing */
+     //   } while (5==4);
+     //   for (;;) {
+      //      try {
+     //           if (true) { continue; }
+      //          throw new SomeException();
+     //       }
+     //       catch (SomeException e) {
+         //       /* do nothing */
+     //       }
+     //   }
+
+        //final int x;
+    } 
+}
